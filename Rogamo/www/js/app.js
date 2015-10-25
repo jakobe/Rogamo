0f49dc0ae569b166a688acb351dcecb8daaa8d02
Jakob Engelbrecht Olesen <jakob@basher.dk>
2015-10-25 13:41:40 +0100

    Added Rogamo app structure (tabs + content)

--- Rogamo/www/js/app.js
+++ Rogamo/www/js/app.js
@@ -4,14 +4,53 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
+.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/')
 
+    $stateProvider.state('home', {
+        url: '/home',
+        views: {
+            home: {
+                templateUrl: 'home.html'
+            }
+        }
+    })
+
+    $stateProvider.state('ar', {
+        url: '/ar',
+        views: {
+            ar: {
+                templateUrl: 'ar.html'
+            }
+        }
+    })
+
+    $stateProvider.state('robot', {
+        url: '/robot',
+        views: {
+            robot: {
+                templateUrl: 'robot.html'
+            }
+        }
+    })
+
+    $stateProvider.state('help', {
+        url: '/help',
+        views: {
+            help: {
+                templateUrl: 'help.html'
+            }
+        }
+    })
+})
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
+    $ionicPlatform.ready(function () {
+        console.log("App ready.");
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
+    //if(window.cordova && window.cordova.plugins.Keyboard) {
+    //  cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    //}
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
